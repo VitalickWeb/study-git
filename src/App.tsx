4293dabb76ee24928801f8330273fc4e846f3120
Vitalick <miraigeweb@gmail.com>
2022-07-04 17:17:54 +0300

    Added idea to ignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,24 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import {Header} from './Header';
-import {Body} from './Body';
-import {CarsType, Footer} from './Footer';
-import {UniversalButton} from "./components/button/UniversalButton";
-import {UseState} from "./components/hooks/useState/UseState";
+import {v1} from "uuid";
 
-export type BanknotsType = 'Dollars' | 'RUBLS'
-
-export type MoneyType = {
-    banknots: BanknotsType
-    value: number
-    number: string
-}
 
 const App = () => {
-    //Данные храним в App потому что нам не нужны будут 2 App
-    const topCars: Array<CarsType> = [
+    //input and button
+    let message = [
+        {id: v1(), message: 'message1'},
+        {id: v1(), message: 'message2'},
+        {id: v1(), message: 'message3'},
+    ]
+
+    //Данные храним в App
+    const topCars = [
         {manufacturer: 'BMW', model: 'm5cs'},
         {manufacturer: 'Mercedes', model: 'e63s'},
         {manufacturer: 'Audi', model: 'rs6'}
     ]
 
-    //Прописываем логику на кнопку в компоненте выше
-    const buttonFoo1 = (subscriber: string, age: number) => {
-        console.log(subscriber, age);
-    }
-    const buttonFoo2 = (subscriber: string, age: number) => {
-        console.log(subscriber, age);
-    }
-    const buttonFoo3 = () => {
-        console.log('i am stupped button');
-    }
-
-    const [money, setMoney] = useState<Array<MoneyType>>([
+    const money = [
         {banknots: 'Dollars', value: 100, number: ' a1234567890'},
         {banknots: 'Dollars', value: 50, number: ' z1234567890'},
         {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
@@ -42,47 +27,11 @@ const App = () => {
         {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
         {banknots: 'Dollars', value: 50, number: ' x1234567890'},
         {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
-    ])
-
-    const [filteredCurrency, setFilteredCurrency] = useState<Array<MoneyType>>(money)
-    debugger
-    const onClickHandler = (currency: BanknotsType | 'all' = 'Dollars') => {
-
-        if (currency === 'all') {
-            setFilteredCurrency(money)
-            return
-        }
-        const filteredCurrency =  money.filter((filteredСurrency) => filteredСurrency.banknots === currency)
-        setFilteredCurrency(filteredCurrency)
-    }
-
+    ]
 
     return (
         <div className="App">
-            <Header title={'NEW HEADER'}/>
-            <Body title={'NEW BODY'}/>
-            <Footer cars={topCars}/>
 
-            <div>
-                <UniversalButton
-                    name={'MyYouTubeChanel-1'}
-                    callBack={() => buttonFoo1('Vit here', 41)}
-                />
-                <UniversalButton
-                    name={'MyYouTubeChanel-2'}
-                    callBack={() => buttonFoo2('Liza here', 15)}
-                />
-                <UniversalButton
-                    name={'stupped button'}
-                    callBack={buttonFoo3}
-                />
-            </div>
-
-            <UseState
-                callBack={onClickHandler}
-                denomination={filteredCurrency}
-
-            />
         </div>
     );
 }
